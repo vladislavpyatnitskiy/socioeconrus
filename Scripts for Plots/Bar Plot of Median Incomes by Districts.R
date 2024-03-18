dab9f17b677ea41a59455f21981ff5eea8cc2761
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-18 12:01:53 +0500

    Create Bar Plot of Median Incomes by Districts.R
    Added script generating bar plot of median incomes by districts

---
+++ Scripts for Plots/Bar Plot of Median Incomes by Districts.R
@@ -0,0 +1,56 @@
+# Median Income across Federal Districts
+bar.plt.soc.group.i <- function(x, main = NULL){
+  
+  pct <- data.frame(x[,2], as.numeric(x[,3])) # Create Separate Data Frame
+  
+  colnames(pct) <- c("Federal District", "Income") # Column names
+  
+  # Aggregate Median income data to get median values for each district
+  pct <- aggregate(Income ~ `Federal District`, data=pct, median)
+  
+  v <- pct[,2] # Make a vector
+  
+  names(v) <- pct[,1] # assign names
+  
+  # Colours
+  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
+               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
+               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
+               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
+               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
+               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
+               "#bd5975")
+  
+  v <- sort(v, decreasing = T) # Sort values
+  
+  mx <- ceiling(round(max(v)) / 10 ^ (nchar(round(max(v))) - 1)) *
+    10 ^ (nchar(round(max(v))) - 1) # Round maximum value up
+    
+  mn <- trunc(round(min(v)) / 10 ^ (nchar(round(min(v))) - 1)) *
+    10 ^ (nchar(round(min(v))) - 1) # Round minimum value up
+  
+  p.seq <- seq(mn, mx, by = 20) # Values for axes
+  
+  bar.plt.script <- barplot(v, las = 1, ylim = c(min(v) - 10, max(v) + 10),
+                            xpd = F, col = colors37, main = main) # Plot
+  
+  for (n in p.seq){ abline(h = n, col ="grey",lty = 3) } # Put horiz lines
+  abline(v = bar.plt.script, col ="grey",lty = 3) # Put vertical lines
+  abline(h = mean(v), col = "red", lwd = 3) # Mean percentage line
+  abline(h = median(v), col = "green", lwd = 3) # Median percentage line
+  
+  # Legend for Mean and Median lines
+  legend("topright", legend=c((sprintf("Mean:    US$ %s",round(mean(v),2))),
+                              sprintf("Median: US$ %s", round(median(v),2))),
+         fill = c("red", "green"), cex = .8, bty = "n")
+  
+  par(mar = c(4, 4, 4, 4)) # Define borders of the plot
+  
+  axis(side = 2, at = p.seq, las = 1) # Axes
+  axis(side = 4, at = p.seq, las = 1)
+  
+  box() # Put Bar Plot into box
+}
+bar.plt.soc.group.i(rus.bubble.df1,
+                    main =
+                      "Bar Plot of Median Incomes across Federal Districts") 
