0bd5abffac0f2641d2787dcbc1c2ff126da30fa4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-29 11:37:51 +0500

    Update Income Histogram.R
    Updated script:
    
    • Changed title of histogram
    • Added right y-axis

--- Scripts for Plots/Income Histogram.R
+++ Scripts for Plots/Income Histogram.R
@@ -1,4 +1,4 @@
-hist.plt.soc <- function(x, main = NULL){ # Histogram Plot
+hist.plt.soc <- function(x){ # Histogram Plot of Regions by Income
   
   x <- as.numeric(x) # make values numeric
   
@@ -12,16 +12,21 @@ hist.plt.soc <- function(x, main = NULL){ # Histogram Plot
   p.seq <- seq(s.max / 20, s.max, s.max / 20) # values for x-axis
   
   # Plot
-  hist(x, freq = T, xlab = "Histogram & Normal Distribution", border = "white",
-       xlim = c(s.min, s.max), col = "steelblue", las = 1,
-       breaks = round(length(x), -1), main = main)
+  hist(x, freq = T, xlab = "Income by Monthly GDP per Capita in US$",
+       border = "white", xlim = c(s.min, s.max), col = "steelblue", las = 1,
+       breaks = round(length(x), -1),
+       main = "Histogram of Russian Regions by GDP per Capita in US$")
   
   for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } #
-    
-  abline(h = 0, col = "black") # Add vertical line at y = 0
+  
+  abline(h = 0) # Add vertical line at y = 0
   axis(side = 1, at = p.seq) # Set x-axis values
   
   for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) } # y-axis
+  
+  for (n in 1:2){ axis(side = 2 * n, at = seq(100, 0), las = 1) } # Set y-axes
+  
+  par(mar = c(4, 4, 4, 4)) # Define borders of the plot
   
   # Add Normal Distribution
   lines(seq(s.min, s.max, by = 1),
@@ -30,5 +35,4 @@ hist.plt.soc <- function(x, main = NULL){ # Histogram Plot
   
   box() # Define borders
 }
-hist.plt.soc(as.numeric(rus.bubble.df1[,3]),
-             main = "Histogram of Russian Regions") # Test
+hist.plt.soc(rus.bubble.df1[,3]) # Test
