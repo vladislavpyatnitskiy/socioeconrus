e87c941cfbddecc68677c1673dfa3a876792b4c2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-03-29 12:41:48 +0500

    Update Histogram of Life Expectancies.R
    Updated plot by adding y-axis on the right

--- Scripts for Plots/Histogram of Life Expectancies.R
+++ Scripts for Plots/Histogram of Life Expectancies.R
@@ -1,4 +1,4 @@
-hist.plt.year <- function(x, main = NULL){ # Histogram Plot
+hist.plt.year <- function(x){ # Histogram Plot of Regions by Life Expectancy
   
   x <- as.numeric(x) # Numeric
   
@@ -9,15 +9,15 @@ hist.plt.year <- function(x, main = NULL){ # Histogram Plot
   
   # Plot
   hist(x, freq = T, xlab = "Years", border = "white", xlim = c(s.min, s.max),
-       col = "steelblue", las = 1, breaks = round(length(x), -1), main = main)
+       col = "steelblue", las = 1, breaks = round(length(x), -1),
+       main = "Histogram of Life Expectancies by Russian Regions")
   
   for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # Vertical line
-  
-  abline(h = 0, col = "black") # Add vertical line at y = 0
-  axis(side = 1, at = p.seq) # Set x-axis values
-  axis(side = 2, at = seq(1, 100, 1), las = 2) # Set y-axis values
-  
   for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) } # y-axis
+  
+  abline(h = 0) # Add vertical line at y = 0
+  axis(side = 1, at = p.seq) # Set x-axis values
+  for (n in 1:2){ axis(side = 2*n,at=seq(100,from=0,by=1),las=2) } # Set y-axes
   
   # Add Normal Distribution
   lines(seq(round(s.min, -1), s.max, by = 0.01),
@@ -26,5 +26,4 @@ hist.plt.year <- function(x, main = NULL){ # Histogram Plot
   
   box() # Define borders
 }
-hist.plt.year(rus.bubble.df1[,4],
-              main = "Histogram of Life Expectancies by Russian Regions") 
+hist.plt.year(rus.bubble.df1[,4]) 
