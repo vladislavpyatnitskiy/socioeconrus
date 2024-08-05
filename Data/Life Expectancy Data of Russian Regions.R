6bdfdec4c308de3fbbdaa7d55b5f0dcb7de87844
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-05 11:28:27 +0500

    Update Life Expectancy Data of Russian Regions.R
    Optimised script:
    
    • Reduced excessive lines
    • Substituted FOR loop with Sequence

--- Data/Life Expectancy Data of Russian Regions.R
+++ Data/Life Expectancy Data of Russian Regions.R
@@ -1,33 +1,24 @@
 library("rvest") # Library
 
 life.exp.ru <- function(x){ # Life Expectancy of Russian regions data 
- 
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[1]] -> tab # Assign Table 
+  y <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[1]]  %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text() # Get data from HTML
   
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # Get data of regions with life expectancies
-  
-  for (n in 1:(length(y) / 20)){ d <- y[1 + n * 20] # Region names
-    
-    v <- rbind.data.frame(v, cbind(d, as.numeric(y[2 + n * 20]))) }
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
+  v <- data.frame(y[seq(from = 1, to = length(y), by = 20)], # Region name
+                  y[seq(from = 2, to = length(y), by = 20)]) # Life Expectancy
   
   colnames(v) <- c("Region", "Life Expectancy") # Column names
-  
-  if (isTRUE(any(v[,1] == "Sakha (Yakutia)"))){ # Rename some serions
     
-    v[v$Region == "Khanty-Mansi AO(Tyumen Oblast)",][,1] <- "Khanty-Mansi AO" 
-    v[v$Region == "Sakha (Yakutia)",][,1] <- "Sakha" 
-    v[v$Region == "Arkhangelsk Oblast(except AO)",][,1] <- "Arkhangelsk Oblast"
-    v[v$Region == "Yamalo-Nenets AO(Tyumen Oblast)",][,1] <- "Yamalo-Nenets AO"
-    v[v$Region == "Jewish Autonomous Oblast",][,1] <- "Jewish AO" 
-    v[v$Region == "Nenets AO(Arkhangelsk Oblast)",][,1] <- "Nenets AO" 
-    v[v$Region == "Tyumen Oblast(except two AO)",][,1] <- "Tyumen Oblast" 
-  }
+  v[v$Region == "Khanty-Mansi AO(Tyumen Oblast)",][,1] <- "Khanty-Mansi AO" 
+  v[v$Region == "Sakha (Yakutia)",][,1] <- "Sakha" 
+  v[v$Region == "Arkhangelsk Oblast(except AO)",][,1] <- "Arkhangelsk Oblast"
+  v[v$Region == "Yamalo-Nenets AO(Tyumen Oblast)",][,1] <- "Yamalo-Nenets AO"
+  v[v$Region == "Jewish Autonomous Oblast",][,1] <- "Jewish AO" 
+  v[v$Region == "Nenets AO(Arkhangelsk Oblast)",][,1] <- "Nenets AO" 
+  v[v$Region == "Tyumen Oblast(except two AO)",][,1] <- "Tyumen Oblast" 
+  
   v # Display
 }
 life.exp.ru("List_of_federal_subjects_of_Russia_by_life_expectancy") # Test
