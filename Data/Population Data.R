786e8caa2e46eeccf74dfb77d9a9ce7703f97f6f
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-09-04 10:50:17 +0500

    Update Population Data.R
    Optimised script:
    
    • Reduced excessive lines
    • Cleaned regions' names

--- Data/Population Data.R
+++ Data/Population Data.R
@@ -2,11 +2,9 @@ library("rvest") # Library
 
 pop.ru <- function(x){ # Russian Regions by life expectancy
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
-  
-  s.wiki <- s %>% html_nodes('table') %>% .[[1]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+  y <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text()
   
   v <- NULL # variables to store data frames
   
@@ -22,17 +20,24 @@ pop.ru <- function(x){ # Russian Regions by life expectancy
   
   v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
   
-  if (isTRUE(any(v[,1] == "Komi Republic"))){ # Change names
+  v[v$Region == "Moscow",][,1] <- "Moscow City" # Distinguish city from oblast
+  
+  for (n in 1:nrow(v)) if (isTRUE(grepl("Oblast", v[n,1]))){ # Reduce "Oblast"
     
-    v[v$Region == "Komi Republic",][,1] <- "Komi"
-    v[v$Region == "Khanty–Mansi A.O. (Yugra)",][,1] <- "Khanty-Mansi AO"
-    v[v$Region == "Sakha (Yakutia)",][,1] <- "Sakha"
-    v[v$Region == "Arkhangelsk Oblast[a]",][,1] <- "Arkhangelsk Oblast"
-    v[v$Region == "North Ossetia–Alania",][,1] <- "North Ossetia"
-    v[v$Region == "Yamalo Nenets A.O.",][,1] <- "Yamalo-Nenets AO"
-    v[v$Region == "Jewish Autonomous Oblast",][,1] <- "Jewish AO"
-    v[v$Region == "Nenets Autonomous Okrug",][,1] <- "Nenets AO"
-    }
+      O <- read.fwf(textConnection(v[n,1]), widths = c(nchar(v[n,1]) - 7, 1),
+                    colClasses = "character")[,1] 
+      
+      v[v$Region == v[n,1],][,1] <- O } 
+    
+  v[v$Region == "Komi Republic",][,1] <- "Komi"
+  v[v$Region == "Khanty–Mansi A.O. (Yugra)",][,1] <- "Khanty-Mansi AO"
+  v[v$Region == "Sakha (Yakutia)",][,1] <- "Sakha"
+  v[v$Region == "Arkhangelsk Ob",][,1] <- "Arkhangelsk"
+  v[v$Region == "North Ossetia–Alania",][,1] <- "North Ossetia"
+  v[v$Region == "Yamalo Nenets A.O.",][,1] <- "Yamalo-Nenets AO"
+  v[v$Region == "Jewish Autonomous",][,1] <- "Jewish AO"
+  v[v$Region == "Nenets Autonomous Okrug",][,1] <- "Nenets AO"
+  
   v # Display
 }
 pop.ru("List_of_federal_subjects_of_Russia_by_population") # Test
