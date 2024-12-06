76c5fd0489248952059b3381e7403c1365caedac
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-12-06 11:01:21 +0500

    Update Bar Plot of Life Expectancy Difference across Districts.R
    Optimised script

--- Scripts for Plots/Bar Plot of Life Expectancy Difference across Districts.R
+++ Scripts for Plots/Bar Plot of Life Expectancy Difference across Districts.R
@@ -9,13 +9,12 @@ bar.plt.soc.group.y.diff <- function(x){
   
   names(v) <- pct[,1] # assign names
   
-  # Colours
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
         "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
         "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
         "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
-        "#895c8b","#bd5975")
+        "#895c8b","#bd5975") # Colours
   
   v <- sort(v, decreasing = T) # Sort values
   
@@ -25,18 +24,19 @@ bar.plt.soc.group.y.diff <- function(x){
                main = "Difference of Life Expectancy across Federal Districts", 
                ylab="Difference between Female & Male Life Expectancy") # Plot
   
-  for (n in p.seq){ abline(h = n, col ="grey",lty = 3) } # Put horiz lines
+  for (n in p.seq){ abline(h = n, col = "grey", lty = 3) } # Horizontal lines
   abline(v = B, col ="grey",lty = 3) # Put vertical lines
-  abline(h = mean(v), col = "red", lwd = 3) # Mean percentage line
-  abline(h = median(v), col = "green", lwd = 3) # Median percentage line
+  
+  cols = c("red", "green") # Colours
+  vals = list(list(mean(v), median(v)), cols) # Mean & Median lines
   
   # Legend for Mean and Median lines
-  legend(x = "bottom", inset = c(0, -.175), cex = .85, bty = "n", horiz = T,
+  legend(x="bottom", inset=c(0, -.175), cex=.85, bty="n", horiz=T, col = cols,
          legend = c((sprintf("Mean: %s", round(mean(v), 2))),
-                    sprintf("Median: %s", round(median(v), 2))),
-         col = c("red", "green"), xpd = T, pch = 15)
+                    sprintf("Median: %s", round(median(v), 2))), xpd=T, pch=15)
   
-  for (n in 1:2){ axis(side = 2 * n, at = p.seq, las = 1) } # Set up y-axis
+  for (n in 1:2){ abline(h = vals[[1]][[n]], col = vals[[2]][n], lwd = 3) 
+    axis(side = 2 * n, at = p.seq, las = 1) } # Set up y-axis
   
   par(mar = c(4, 4, 4, 4)) # Define borders of the plot
   
