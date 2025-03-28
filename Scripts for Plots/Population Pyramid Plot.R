79a6445e9e48a1d09b5ca37917e2687fa64e5612
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-03-28 11:15:26 +0500

    Update Population Pyramid Plot.R
    Optimised script:
    
    • Added option to choose country for pyramid
    • Added option to set up country name 
    • Renamed some variables

--- Scripts for Plots/Population Pyramid Plot.R
+++ Scripts for Plots/Population Pyramid Plot.R
@@ -1,8 +1,8 @@
 library("ggplot2") # Library
 
-pyramid.plt.ru <- function(x){ # Population Pyramid of Russia
+pyramid.plt.ru <- function(x, c, name = NULL){ # Population Pyramid of Russia
   
-  y <- sprintf("https://www.populationpyramid.net/api/pp/643/%s/?csv=true", x)
+  y <- sprintf("https://www.populationpyramid.net/api/pp/%s/%s/?csv=true",c,x)
   
   y <- read.csv2(file = url(y)) # Download CSV file from website
   
@@ -14,35 +14,35 @@ pyramid.plt.ru <- function(x){ # Population Pyramid of Russia
   
   colnames(df) <- c("Age", "Male", "Female") # Column names
   
-  df.plt <- NULL
+  D <- NULL # Optimise for ggplot2 format
   
   for (n in 1:nrow(df)){ # Optimise for ggplot2 format
     
-    df.plt <- rbind.data.frame(df.plt, rbind(c(df[n,1], colnames(df)[2],
-                                               as.numeric(df[n,2])),
-                                             c(df[n,1], colnames(df)[3],
-                                               as.numeric(df[n,3])))) }
+    D <- rbind.data.frame(D, rbind(c(df[n,1], colnames(df)[2],
+                                     as.numeric(df[n,2])),
+                                   c(df[n,1], colnames(df)[3],
+                                     as.numeric(df[n,3])))) }
   
   # Calculate percentage of each gender and age group
-  df.plt[,ncol(df.plt) + 1] <- (as.numeric(df.plt[,3]) /
-                                  sum(as.numeric(df.plt[,3]))) * 100
+  D[,ncol(D) + 1] <- (as.numeric(D[,3]) / sum(as.numeric(D[,3]))) * 100
   
-  colnames(df.plt) <- c("Age", "Gender", "Number", "Percent") # Column names
+  colnames(D) <- c("Age", "Gender", "Number", "Percent") # Column names
   
   # Make age factor so it woill not be sorted according to numbers
-  df.plt$Age <- factor(df.plt$Age, level = unique(df.plt$Age), ordered = F)
+  D$Age <- factor(D$Age, level = unique(D$Age), ordered = F)
   
-  df.plt$b <- ifelse(df.plt$Gender == "Male", -1, 1) # Colour column
+  D$b <- ifelse(D$Gender == "Male", -1, 1) # Colour column
   
-  df.plt <- df.plt[order(-df.plt$b), ] # Order in a descending way
+  D <- D[order(-D$b), ] # Order in a descending way
   
-  df.plt[,4] <- df.plt[,4] * df.plt[,5] # 
+  D[,4] <- D[,4] * D[,5] # 
   
-  ggplot(df.plt) +
-    geom_bar(aes(x = Percent, y = Age, fill = Gender), stat = "identity") +
-    labs(title = sprintf("Population Pyramid of Russia in %s", x),
-         x = "Percentage (%)", y = "Age Group") +
+  N <- ifelse(is.null(name), sprintf("Population Pyramid in %s", x),
+              sprintf("Population Pyramid of %s in %s", name, x))
+  
+  ggplot(D) + geom_bar(aes(x=Percent, y=Age, fill=Gender), stat = "identity") +
+    labs(title = N, x = "Percentage (%)", y = "Age Group") +
     scale_x_continuous(breaks = seq(-10, 10, 1)) +
     theme_minimal() + scale_fill_manual(values = c("red4", "steelblue"))
 }
-pyramid.plt.ru("2023")
+pyramid.plt.ru(2025, 643, "Russia")
