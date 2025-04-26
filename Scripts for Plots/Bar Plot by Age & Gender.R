4480e1dc6ac3f5f92356c7806a148c31782dd6a8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-26 14:21:53 +0500

    Update Bar Plot by Age & Gender.R
    Optimised script:
    
    • Reduced excessive lines
    • Rewrote part for grid

--- Scripts for Plots/Bar Plot by Age & Gender.R
+++ Scripts for Plots/Bar Plot by Age & Gender.R
@@ -1,6 +1,6 @@
 bar.plt.soc.age.gender <- function(C, Y){ # Bar plot of gender age groups
   
-  f <- sprintf("https://www.populationpyramid.net/api/pp/%s/%s/?csv=true", C, Y)
+  f <- sprintf("https://www.populationpyramid.net/api/pp/%s/%s/?csv=true",C,Y)
   
   f <- read.csv2(file = url(f)) # Download CSV file to get data
   
@@ -14,16 +14,14 @@ bar.plt.soc.age.gender <- function(C, Y){ # Bar plot of gender age groups
   
   ages <- as.factor(d[,1]) # Change data type of first column to factor
   
-  for (n in 2:3){ d[,n] <- as.numeric(d[,n]) } # make columns numeric
-  
   D <- NULL  
   
   for (n in 1:nrow(d)){ # Optimise for ggplot2 format
     
     D <- rbind.data.frame(D, rbind(c(sprintf("%s %s",d[n,1],colnames(d)[2]),
-                                     as.numeric(d[n,2])),
+                                     d[n,2]),
                                    c(sprintf("%s %s",d[n,1],colnames(d)[3]),
-                                     as.numeric(d[n,3])))) }
+                                     d[n,3]))) }
   A <- D[,1] # Age & Gender groups
   
   N <- as.numeric(D[,2]) # Make numeric format
@@ -34,35 +32,33 @@ bar.plt.soc.age.gender <- function(C, Y){ # Bar plot of gender age groups
   
   m <- round(max(N), 0) + 1 # Set Maximum value for y - axis
   
-  N <- N / (10 ^ (nchar(m) - 1)) # Divide by million
-  
   J <- NULL # Create set of colours for bar plot
   
   for (n in 1:length(N)){ J <- c(J, ifelse(grepl("F", names(N)[n]) == T,
                                            "magenta", "blue")) }
-    
-  B <- barplot(N, names.arg = names(N), las = 2, col = J, xpd = T, 
-               main = "Population by Age & Gender Groups in millions",
-               ylim = c(0, m / (10 ^ (nchar(m) - 1)) + 1))
+  
+  N <- N / (10 ^ (nchar(m) - 1)) # Divide by million
+  
+  B <- barplot(N, las = 2, col = J, xpd = T, ylim = c(0, max(N) + 1),
+               main = "Population by Age & Gender Groups in millions")
   
   abline(v = B, col = "grey", lty = 3) # Vertical lines
+  grid(nx = 1, ny = NULL, lty = 3, col = "grey") # Horizontal lines
   
-  vals = list(list(mean(N), median(N), seq(0, m, .5)), c("red","green","grey"),
-              c(1, 1, 3), c(3, 3, 1))
-
-  axis(side=2, at = seq(0.5, m / (10^(nchar(m) - 1)) + 1, 1), las=1) # Set axes
-  axis(side=4, at = seq(0, m / (10^(nchar(m) - 1)) + 1, 0.5), las=1)
+  vals = list(list(mean(N), median(N)), c("red", "green"), c(1, 1), c(3, 3))
+  
+  axis(side = 4, las = 2) # Right y-axis
   
   par(mar = c(10, 5, 3, 5)) # Define borders of the plot
   
-  for (n in 1:3){ abline(h = vals[[1]][[n]], col = vals[[2]][n],
+  for (n in 1:2){ abline(h = vals[[1]][[n]], col = vals[[2]][n],
                          lty = vals[[3]][n], lwd = vals[[4]][n]) }
   
-  legend(x = "bottom", inset = c(0, -0.6), cex = .85, bty = "n", horiz = T, 
+  legend(x = "bottom", inset = c(0, -0.42), cex = 1, bty = "n", horiz = T, 
          legend = c((sprintf("Mean: %s", round(vals[[1]][[1]], 2))),
                     sprintf("Median: %s", round(vals[[1]][[2]], 2))),
          col = vals[[2]][c(1,2)], xpd = T, pch = 15)
   
   box() # Box
 }
-bar.plt.soc.age.gender(C = "643", Y = "2024") # Test
+bar.plt.soc.age.gender(C = "643", Y = "2025") # Test
