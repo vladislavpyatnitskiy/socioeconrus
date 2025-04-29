484fa3338c1724eec07bc70cf1dedcdbd93f5f8b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-29 10:54:19 +0500

    Update Bar Plot of Life Expectancy Difference across Districts.R
    Optimised script:
    
    • Reduced excessive lines
    • Renamed variables
    • Improved part for grid

--- Scripts for Plots/Bar Plot of Life Expectancy Difference across Districts.R
+++ Scripts for Plots/Bar Plot of Life Expectancy Difference across Districts.R
@@ -1,13 +1,11 @@
-bar.plt.soc.group.y.diff <- function(x){
-  
-  pct <- x # Create Separate Data Frame
+bar.plt.soc.group.y.diff <- function(x){ # Difference in life expectancy
   
   # Aggregate life expectancy data to get median values for each district
-  pct <- aggregate(`Difference` ~ `Federal District`, data=pct, median)
+  x <- aggregate(`Difference` ~ `Federal District`, data = x, median)
   
-  v <- pct[,2] # Make a vector
+  v <- x[,2] # Make a vector
   
-  names(v) <- pct[,1] # assign names
+  names(v) <- x[,1] # assign names
   
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
@@ -18,27 +16,26 @@ bar.plt.soc.group.y.diff <- function(x){
   
   v <- sort(v, decreasing = T) # Sort values
   
-  p.seq <- seq(round(min(v) - 1), round(max(v) + 1), by = 1) # Values for axes
-  
   B <- barplot(v, las = 1, ylim = c(min(v) - 1, max(v) + 1), xpd = F, col = C,
                main = "Difference of Life Expectancy across Federal Districts", 
-               ylab="Difference between Female & Male Life Expectancy") # Plot
+               sub="Note: Difference between Female & Male Life Expectancy") 
   
-  for (n in p.seq){ abline(h = n, col = "grey", lty = 3) } # Horizontal lines
-  abline(v = B, col ="grey",lty = 3) # Put vertical lines
+  grid(nx = 1, ny = NULL, col = "grey", lty = 3, lwd = 1) # Horizontal lines
+  abline(v = B, col ="grey",lty = 3) # Vertical lines
   
   cols = c("red", "green") # Colours
-  vals = list(list(mean(v), median(v)), cols) # Mean & Median lines
+  vals = list(mean(v), median(v)) # Mean & Median lines
   
   # Legend for Mean and Median lines
   legend(x="bottom", inset=c(0, -.175), cex=.85, bty="n", horiz=T, col = cols,
          legend = c((sprintf("Mean: %s", round(mean(v), 2))),
                     sprintf("Median: %s", round(median(v), 2))), xpd=T, pch=15)
   
-  for (n in 1:2){ abline(h = vals[[1]][[n]], col = vals[[2]][n], lwd = 3) 
-    axis(side = 2 * n, at = p.seq, las = 1) } # Set up y-axis
+  for (n in 1:2){ abline(h = vals[n], col = cols[n], lwd = 3) }
   
-  par(mar = c(4, 4, 4, 4)) # Define borders of the plot
+  axis(side = 4, las = 2) # Right y-axis
+  
+  par(mar = c(6, 4, 4, 4)) # Define borders of the plot
   
   box() # Put Bar Plot into box
 }
