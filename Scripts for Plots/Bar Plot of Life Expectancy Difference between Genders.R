0d19b754a8b3e2a489daacda212f220cab40c0cd
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-04-30 11:18:36 +0500

    Update Bar Plot of Life Expectancy Difference between Genders.R
    Optimised script:
    
    • Reduced excessive lines
    • Rewrote part for grid

--- Scripts for Plots/Bar Plot of Life Expectancy Difference between Genders.R
+++ Scripts for Plots/Bar Plot of Life Expectancy Difference between Genders.R
@@ -1,10 +1,10 @@
-bar.plt.soc.dif <- function(x, main = NULL){ # Life Expectancy across Regions
+bar.plt.soc.dif <- function(x){ # Life Expectancy across Regions
   
-  pct <- data.frame(x) # Create Separate Data Frame
+  x <- data.frame(x) # Create Separate Data Frame
   
-  v <- pct[,2] # Make a vector
+  v <- x[,2] # Make a vector
   
-  names(v) <- pct[,1] # assign names
+  names(v) <- x[,1] # assign names
   
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
@@ -15,26 +15,23 @@ bar.plt.soc.dif <- function(x, main = NULL){ # Life Expectancy across Regions
   
   v <- sort(v, decreasing = T) # Sort values
   
-  p.seq <- seq(round(min(v) - 1), round(max(v) + 1), by = 1) # Values for axes
+  B <- barplot(v, las = 2, ylim = c(min(v) - 1, max(v) + 1), xpd = F, col = C,
+               main = "Life Expectancy Difference between Female & Male")
   
-  bar.plt.script <- barplot(v, las = 2, ylim = c(min(v) - 1, max(v) + 1),
-                            xpd = F, col = C, main = main) # Plot
-  
-  for (n in p.seq){ abline(h = n, col ="grey",lty = 3) } # Put horiz lines
-  abline(v = bar.plt.script, col ="grey",lty = 3) # Put vertical lines
+  grid(nx = 1, ny = NULL, col = "grey", lty = 3, lwd = 1) # Horizontal lines
+  abline(v = B, col = "grey", lty = 3) # Vertical lines
   abline(h = mean(v), col = "red", lwd = 3) # Mean percentage line
   abline(h = median(v), col = "green", lwd = 3) # Median percentage line
   
-  legend(x = "bottom", inset = c(0, -.65), cex = .85, bty = "n", horiz = T,
+  legend(x = "bottom", inset = c(0, -.55), cex = .85, bty = "n", horiz = T,
          legend = c((sprintf("Mean: %s", round(mean(v), 2))),
                     sprintf("Median: %s", round(median(v), 2))),
          col = c("red", "green"), xpd = T, pch = 15)
   
-  for (n in 1:2){ axis(side=2*n, at=seq(round(max(v),0)+1,
-                                        from=round(min(v),0)-1, by=1), las=1) }
+  axis(side = 4, las = 2) # Right y-axis
   
   par(mar = c(12, 3, 3, 3)) # Define borders of the plot
   
   box() # Put Bar Plot into box
 }
-bar.plt.soc.dif(dif.df,main="Life Expectancy Difference between Female & Male") 
+bar.plt.soc.dif(dif.df) # Test
