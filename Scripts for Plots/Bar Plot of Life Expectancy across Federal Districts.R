b4a3c3cfbdc246a0eed8e59a0a1d9d3669c75d9d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-05-01 12:37:02 +0500

    Update Bar Plot of Life Expectancy across Federal Districts.R
    Optimised script:
    
    • Rewrote part for grid
    • Reduced excessive lines
    • Renamed variables

--- Scripts for Plots/Bar Plot of Life Expectancy across Federal Districts.R
+++ Scripts for Plots/Bar Plot of Life Expectancy across Federal Districts.R
@@ -1,50 +1,43 @@
-# Life Expectancy across Federal Districts
-bar.plt.soc.group.y <- function(x, main = NULL){
+bar.plt.soc.group.y <- function(x){ # Life Expectancy across Federal Districts
   
-  pct <- data.frame(x[,2], as.numeric(x[,4])) # Create Separate Data Frame
+  x <- data.frame(x[,2], as.numeric(x[,4])) # Create Separate Data Frame
   
-  colnames(pct) <- c("Federal District", "Life Expectancy") # Column names
+  colnames(x) <- c("Federal District", "Life Expectancy") # Column names
   
   # Aggregate life expectancy data to get median values for each district
-  pct <- aggregate(`Life Expectancy` ~ `Federal District`, data=pct, median)
+  x <- aggregate(`Life Expectancy` ~ `Federal District`, data = x, median)
   
-  v <- pct[,2] # Make a vector
+  v <- x[,2] # Make a vector
   
-  names(v) <- pct[,1] # assign names
+  names(v) <- x[,1] # assign names
   
-  # Colours
-  colors37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d",
-               "#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c",
-               "#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d",
-               "#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3",
-               "#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d",
-               "#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b",
-               "#bd5975")
+  C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
+        "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
+        "#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979",
+        "#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672",
+        "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
+        "#895c8b","#bd5975") # Colours
   
   v <- sort(v, decreasing = T) # Sort values
   
-  p.seq <- seq(round(min(v) - 1), round(max(v) + 1), by = 1) # Values for axes
+  B <- barplot(v, las = 1, ylim = c(min(v) - 1, max(v) + 1), xpd = F, col = C,
+               main = "Bar Plot of Life Expectancy across Federal Districts") 
   
-  bar.plt.script <- barplot(v, las = 1, ylim = c(min(v) - 1, max(v) + 1),
-                            xpd = F, col = colors37, main = main) # Plot
-  
-  for (n in p.seq){ abline(h = n, col ="grey",lty = 3) } # Put horiz lines
-  abline(v = bar.plt.script, col ="grey",lty = 3) # Put vertical lines
+  grid(nx = 1, ny = NULL, col = "grey", lty = 3, lwd = 1) # Horizontal lines
+  abline(v = B, col ="grey",lty = 3) # Put vertical lines
   abline(h = mean(v), col = "red", lwd = 3) # Mean percentage line
   abline(h = median(v), col = "green", lwd = 3) # Median percentage line
   
   # Legend for Mean and Median lines
-  legend(x = "bottom", inset = c(0, -.175), cex = .85, bty = "n", horiz = T,
+  legend(x = "bottom", inset = c(0, -.15), cex = .85, bty = "n", horiz = T,
          legend = c((sprintf("Mean: %s", round(mean(v), 2))),
                     sprintf("Median: %s", round(median(v), 2))),
          col = c("red", "green"), xpd = T, pch = 15)
   
-  for (n in 1:2){ axis(side = 2 * n, at = p.seq, las = 1) } # Set up y-axis
+  axis(side = 4, las = 2) # Right Y-axis
   
   par(mar = c(4, 4, 4, 4)) # Define borders of the plot
   
   box() # Put Bar Plot into box
 }
-bar.plt.soc.group.y(rus.bubble.df1,
-                    main =
-                      "Bar Plot of Life Expectancy across Federal Districts") 
+bar.plt.soc.group.y(rus.bubble.df1) # Test
