46131995922dd27598c5239bad5e61185e3bc74b
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-05-04 10:35:19 +0500

    Update Bar Plot of Population by Districts.R
    Optimised script:
    
    • Reduced excessive lines
    • Improved part for grid
    • Rewrote part for plot and legend

--- Scripts for Plots/Bar Plot of Population by Districts.R
+++ Scripts for Plots/Bar Plot of Population by Districts.R
@@ -1,22 +1,14 @@
 bar.plt.soc.group <- function(x){ # Bar Plot of Population by Districts
   
-  options(scipen = 1000000) # Change format to numeric
+  x <- aggregate(Population ~ `Federal District`, data = x, sum) # Sum
   
-  pct <- data.frame(x[,2], x[,5]) # Federal District and Population data
+  regions <- x[,1] # Make Federal District names as row names
   
-  colnames(pct) <- c("Federal District", "Population") # Assign column names
-  
-  pct <- aggregate(Population ~ `Federal District`, data=pct, sum) # Sum
-  
-  regions <- pct[,1] # Make Federal District names as row names
-  
-  pct <- pct[,-1] # Reduce excessive names 
+  pct <- x[,-1] # Reduce excessive names 
   
   names(pct) <- regions # Put names 
   
-  pct <- sort(pct, decreasing = T) / 1000000 # Sort values
-  
-  p.seq <- seq(0, max(pct), round(max(pct), -1) / 8) # Set axes values
+  pct <- sort(pct, decreasing = T) / 10 ^ 6 # Sort values
   
   C = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc",
         "#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a",
@@ -25,27 +17,46 @@ bar.plt.soc.group <- function(x){ # Bar Plot of Population by Districts
         "#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7",
         "#895c8b","#bd5975") # Colours
   
-  # Bar Plot
-  B <- barplot(pct, ylim = c(0, max(pct) + 1), names.arg = names(pct), col = C,
-               las=1, main = "Population among Federal Districts in millions")
+  B <- barplot(
+    pct,
+    ylim = c(min(pct) - 1, max(pct) + 1),
+    col = C,
+    las = 1,
+    xpd = F,
+    main = "Population among Federal Districts in millions"
+    )
   
   par(mar = c(4, 4, 4, 4)) # Define borders of the plot
   
-  for (n in p.seq){ abline(h = n, col ="grey",lty = 3) } # Put horizontal lines
+  grid(nx = 1, ny = NULL, col = "grey", lty = 3, lwd = 1) # Horizontal lines
   abline(v = B, col = "grey", lty = 3) # Put vertical lines
   
   abline(h = mean(pct), col = "red", lwd = 3) # Mean percentage line
   abline(h = median(pct), col = "green", lwd = 3) # Median percentage line
   
-  # Legend for Mean and Median lines
-  legend("bottom",xpd=T,legend=c((sprintf("Mean: %s millions",
-                                          round(mean(pct),2))),
-                                 sprintf("Median: %s millions",
-                                         round(median(pct),2))),
-         col=c("red","green"),pch=15,cex=.8,bty="n",horiz=T,inset=c(0,-.165)) 
+  legend(
+    "bottom",
+    xpd = T,
+    col = c("red", "green"),
+    pch = 15,
+    cex = .8,
+    bty = "n",
+    horiz = T,
+    inset = c(0,-.165),
+    legend = c(
+      (
+        sprintf(
+          "Mean: %s millions", round(mean(pct), 2)
+          )
+        ),
+      sprintf(
+        "Median: %s millions", round(median(pct), 2)
+        )
+      )
+    ) 
   
-  for (n in 1:2){ axis(side = 2 * n, at = p.seq, las = 1) } # Axes
+  axis(side = 4, las = 2)
   
   box() # Put Plot in Box
 }
-bar.plt.soc.group(rus.bubble.df1) # Test
+bar.plt.soc.group(rus.bubble.df1[,c(2,5)]) # Test
