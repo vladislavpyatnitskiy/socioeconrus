d012e994b13e88652c2b13d2056a5d6ef34a7c9a
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-05-06 11:04:59 +0500

    Update Histogram of Fertility Rates.R
    Optimised script:
    
    • Rewrote parts for grid, plot and line for normal distribution
    • Renamed variables
    • Reduced excessive lines

--- Scripts for Plots/Histogram of Fertility Rates.R
+++ Scripts for Plots/Histogram of Fertility Rates.R
@@ -1,29 +1,40 @@
-hist.plt.fert <- function(x, main = NULL){ # Histogram Plot
+hist.plt.fert <- function(x){ # Histogram Plot of Fertility by Regions
+  
+  x <- x[,"Fertility Rate"]
   
   s.min <- min(x) # Minimum value
   s.max <- max(x) # Maximum value
   
-  p.seq <- seq(round(s.min, -1), s.max, by = .1) # values for x-axis
+  hist(
+    x,
+    freq = T,
+    xlab = "Fertility Per Woman",
+    border = "white",
+    main = "Histogram of Fertility Rate by Regions",
+    xlim = c(s.min, s.max),
+    col = "steelblue",
+    breaks = round(length(x), -1),
+    las = 1
+    )
   
-  # Plot
-  hist(x, freq = T, xlab = "Fertility Per Woman", border = "white", main = main,
-       xlim = c(s.min, s.max), col = "steelblue", breaks = round(length(x), -1),
-       las = 1)
+  grid(nx = NULL, ny = NULL, col = "grey", lty = 3, lwd = 1) # Grid
   
-  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # Vertical line
+  abline(h = 0) # Add vertical line at y = 0
   
-  abline(h = 0, col = "black") # Add vertical line at y = 0
-  axis(side = 1, at = p.seq) # Set x-axis values
-  axis(side = 2, at = seq(1, 100, 1), las = 2) # Set y-axis values
-  axis(side = 4, at = seq(0, 100, 1), las = 2) # Set y-axis values
+  axis(side = 4, las = 2) # Set right y-axis 
   
-  for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) } # y-axis
+  R <- seq(round(s.min, -1), s.max, by = 0.01) # Range for Normal Distribution
   
   # Add Normal Distribution
-  lines(seq(round(s.min, -1), s.max, by = 0.01),
-        dnorm(seq(round(s.min, -1), s.max, by = 0.01),
-              mean = mean(x), sd = sd(x)) * s.max, col = "red3", lwd = 2)
+  lines(
+    R,
+    dnorm(R, mean = mean(x), sd = sd(x)) * s.max,
+    col = "red3",
+    lwd = 3
+    )
+  
+  par(mar = rep(4, 4)) # Define borders of the plot
   
   box() # Define borders
 }
-hist.plt.fert(fer.ru.df[,3], main = "Histogram of Fertility Rate by Regions") 
+hist.plt.fert(rus.bubble.df.new) 
