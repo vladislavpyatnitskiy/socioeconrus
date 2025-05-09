9717b0355a30659b8264d9ceb3a65857b68228f8
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-05-09 11:07:16 +0500

    Update Histogram of Population.R
    Optimised script:
    
    • Rewrote part for plot, grid and normal distribution
    • Reduced excessive lines

--- Scripts for Plots/Histogram of Population.R
+++ Scripts for Plots/Histogram of Population.R
@@ -1,31 +1,39 @@
-hist.plt.pop <- function(x, main = NULL){ # Histogram Plot of Population
+hist.plt.pop <- function(x){ # Histogram Plot of Population
   
-  x <- x[,5] / 10 ^ 6 # Divide by 1 million
+  x <- x[,"Population"] / 10 ^ 6 # Divide by 1 million
   
   s.min <- min(x) # Minimum value
   s.max <- max(x) # Maximum value
   
-  p.seq <- seq(0, round(s.max, 0) + 1, by = 1) # values for x-axis
+  hist(
+    x,
+    freq = T,
+    xlab = "Population in millions",
+    border = "white",
+    las = 1,
+    main = "Histogram of Population by Regions",
+    col = "steelblue",
+    breaks = round(length(x), -1), 
+    xlim = c(s.min, round(s.max, 0) + 1)
+    ) # Plot
   
-  # Plot
-  hist(x, freq = T, xlab = "Population in millions", border = "white", las = 1,
-       main = main, col = "steelblue", breaks = round(length(x), -1), 
-       xlim = c(s.min, round(s.max, 0) + 1))
+  grid(nx = NULL, ny = NULL, col = "grey", lty = 3, lwd = 1) # Grid
   
-  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } # Vertical line
+  abline(h = 0) # Add vertical line at y = 0
   
-  abline(h = 0, col = "black") # Add vertical line at y = 0
-  axis(side = 1, at = p.seq) # Set x-axis values
-  axis(side = 2, at = seq(1, 100, 1), las = 2) # Set y-axis values
-  axis(side = 4, at = seq(0, 100, 1), las = 2) # Set y-axis values
+  axis(side = 4, las = 2) # Set y-axis values
   
-  for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) } # y-axis
+  R <- seq(round(s.min, -1), s.max, by = 0.01)
   
-  # Add Normal Distribution
-  lines(seq(round(s.min, -1), s.max, by = 0.01),
-        dnorm(seq(round(s.min, -1), s.max, by = 0.01),
-              mean = mean(x), sd = sd(x)) * s.max * 2, col = "red3", lwd = 2)
+  lines(
+    R,
+    dnorm(R, mean = mean(x), sd = sd(x)) * s.max * 2,
+    col = "red3",
+    lwd = 2
+    ) # Add Normal Distribution
+  
+  par(mar = rep(4, 4)) # Define borders of the plot
   
   box() # Define borders
 }
-hist.plt.pop(rus.bubble.df1, main = "Histogram of Population by Regions") 
+hist.plt.pop(rus.bubble.df1) # Test
