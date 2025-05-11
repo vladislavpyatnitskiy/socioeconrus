94c54817d0f20c64681e7388f568a912046be803
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-05-11 09:50:04 +0500

    Update Income Histogram.R
    Optimised script:
    
    • Reduced excessive lines
    • Improved parts for plot, grid and normal distribution
    • Renamed variables

--- Scripts for Plots/Income Histogram.R
+++ Scripts for Plots/Income Histogram.R
@@ -1,38 +1,41 @@
-hist.plt.soc <- function(x){ # Histogram Plot of Regions by Income
+hist.plt.income <- function(x){ # Histogram Plot of Regions by Income
   
-  x <- as.numeric(x) # make values numeric
+  x <- as.numeric(x[,"Income"]) # make values numeric
   
-  s.min <- min(x) # Minimum value
-  s.max <- max(x) # Maximum value
+  s.min <- min(x) # Value minimum
+  s.max <- max(x) # Value maximum
   
-  # Round maximum values
-  s.max <- ceiling(round(s.max) / 10 ^ (nchar(round(s.max)) - 1)) *
-    10 ^ (nchar(round(s.max)) - 1)
+  hist(
+    x,
+    freq = T,
+    xlab = "Income by Monthly GDP per Capita in US$",
+    border = "white",
+    xlim = c(s.min, s.max),
+    col = "steelblue",
+    las = 1,
+    breaks = round(length(x), -1),
+    main = "Histogram of Russian Regions by GDP per Capita in US$"
+    ) # Plot
   
-  p.seq <- seq(s.max / 20, s.max, s.max / 20) # values for x-axis
+  grid(nx = NULL, ny = NULL, col = "grey", lty = 3, lwd = 1) # Grid
   
-  # Plot
-  hist(x, freq = T, xlab = "Income by Monthly GDP per Capita in US$",
-       border = "white", xlim = c(s.min, s.max), col = "steelblue", las = 1,
-       breaks = round(length(x), -1),
-       main = "Histogram of Russian Regions by GDP per Capita in US$")
-  
-  for (n in p.seq){ abline(v = n, col = "grey", lty = 3) } #
+  abline(v = mean(x), col = "gold", lty = 8, lwd = 3) # Mean (average) value
   
   abline(h = 0) # Add vertical line at y = 0
-  axis(side = 1, at = p.seq) # Set x-axis values
   
-  for (n in seq(1, 100, 1)){ abline(h = n, col = "grey", lty = 3) } # y-axis
+  axis(side = 4, las = 2) # Right Y-axis
   
-  for (n in 1:2){ axis(side = 2 * n, at = seq(100, 0), las = 1) } # Set y-axes
+  par(mar = rep(4, 4)) # Define borders of the plot
   
-  par(mar = c(4, 4, 4, 4)) # Define borders of the plot
+  R <- seq(s.min, s.max, by = 1) # Range
   
-  # Add Normal Distribution
-  lines(seq(s.min, s.max, by = 1),
-        dnorm(seq(s.min, s.max, by = 1), mean = mean(x),
-              sd = sd(x)) * max(x) * 2, col = "red3", lwd = 2)
+  lines(
+    R,
+    dnorm(R, mean = mean(x), sd = sd(x)) * max(x) * 2,
+    col = "red3",
+    lwd = 2
+    ) # Normal Distribution
   
   box() # Define borders
 }
-hist.plt.soc(rus.bubble.df1[,3]) # Test
+hist.plt.income(rus.bubble.df1) # Test
