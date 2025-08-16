381966b9e0973dd09c980bc789120a24141f1881
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-08-16 17:59:41 +0500

    Update Bubble Plot 3D of Impact of Income and Life Exp Difference on Fertility.R
    Rewrote script

--- Scripts for Plots/Bubble Plot 3D of Impact of Income and Life Exp Difference on Fertility.R
+++ Scripts for Plots/Bubble Plot 3D of Impact of Income and Life Exp Difference on Fertility.R
@@ -1,14 +1,25 @@
 library("plotly")
 
 bubble.plt.soc.3d <- function(x){ # Bubble Plot
-
-  fig <- plot_ly(x, x = ~x[,4], y = ~x[,5], z = ~x[,3], size=~x[,7], 
-                 color=~x[,2],marker=list(symbol='circle',sizemode='diameter'),
-                 sizes = c(5, 150), text=~paste(x[,1]))
-  # Rename axes
-  fig %>% layout(title="Impact of Income and Life Exp Difference on Fertility",
-                 scene=list(xaxis=list(title='Life Expectancy Difference'),
-                            yaxis=list(title='Monthly GDP per Capital'),
-                            zaxis=list(title = 'Fertility Rate per Woman')))
+  
+  fig <- plot_ly(
+    x,
+    x = ~x[,"Difference"],
+    y = ~x[,"Income"],
+    z = ~x[,"Fertility Rate"],
+    size=~x[,"Population"],
+    color=~x[,"Federal District"],
+    marker=list(symbol='circle',sizemode='diameter'),
+    sizes = c(5, 150),
+    text=~paste(x[,"Region"])
+    ) %>%
+    layout(
+      title="Impact of Income and Life Exp Difference on Fertility",
+      scene=list(
+        xaxis=list(title='Life Expectancy Difference'),
+        yaxis=list(title='Monthly GDP per Capital'),
+        zaxis=list(title = 'Fertility Rate per Woman')
+        )
+      )
 }
 bubble.plt.soc.3d(df.new.rus) # Test
