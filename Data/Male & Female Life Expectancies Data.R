e03e73f91e89da6a8b0dce0fe8ab8fda03eae4ba
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-04-24 11:47:50 +0500

    Refactor dif.life.exp.ru function for clarity

--- Data/Male & Female Life Expectancies Data.R
+++ Data/Male & Female Life Expectancies Data.R
@@ -1,14 +1,18 @@
 library("rvest") # library
 
-dif.life.exp.ru <- function(x){ # Male & Female Life Expectancies in Russia
+dif.life.exp.ru <- function(){ # Male & Female Life Expectancies in Russia
+  
+  x = "List_of_federal_subjects_of_Russia_by_life_expectancy"
   
   y <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
     html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
     html_nodes('td') %>% html_text()
   
-  v <- data.frame(y[seq(from = 1, to = length(y), by = 20)], # Region name
-                  y[seq(from = 3, to = length(y), by = 20)],
-                  y[seq(from = 4, to = length(y), by = 20)]) # Life Expectancy
+  v <- data.frame(
+    y[seq(from = 1, to = length(y), by = 20)], # Region name
+    y[seq(from = 3, to = length(y), by = 20)],
+    y[seq(from = 4, to = length(y), by = 20)] # Life Expectancy
+    ) 
   
   colnames(v) <- c("Region", "Male", "Female") # Column names
   
@@ -16,11 +20,13 @@ dif.life.exp.ru <- function(x){ # Male & Female Life Expectancies in Russia
   
   for (n in 1:nrow(v)) if (isTRUE(grepl("Oblast", v[n,1]))){ # Reduce "Oblast"
     
-    O <- read.fwf(textConnection(v[n,1]), widths = c(nchar(v[n,1]) - 7, 1),
-                  colClasses = "character")[,1] 
+    O <- read.fwf(
+      textConnection(v[n,1]), widths = c(nchar(v[n,1]) - 7, 1),
+      colClasses = "character"
+      )[,1] 
     
     v[v$Region == v[n,1],][,1] <- O } 
-    
+  
   v[v$Region == "Khanty-Mansi AO(Tyumen ",][,1] <- "Khanty-Mansi AO" 
   v[v$Region == "Sakha (Yakutia)",][,1] <- "Sakha" 
   v[v$Region == "Arkhangelsk Oblast(exc",][,1] <- "Arkhangelsk Oblast"
@@ -31,4 +37,4 @@ dif.life.exp.ru <- function(x){ # Male & Female Life Expectancies in Russia
   
   v # Display
 }
-dif.life.exp.ru("List_of_federal_subjects_of_Russia_by_life_expectancy") # Test
+dif.life.exp.ru() # Test
